fc3389a23c6459363657c87acd9636f5cc06bddb
Gomes <josuegsj1@gmail.com>
2025-02-19 22:01:09 -0300

    Menu and Start

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Stopwath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+            var data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            Console.WriteLine(type);
+        }
+        static void Start(int time)
+        {
+            
+            var currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwath finalizado");
+        }
+    }   
+}
