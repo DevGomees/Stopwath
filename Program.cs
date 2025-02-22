da4399642b870f5553110f876d42fc4c754b6b5e
Gomes <josuegsj1@gmail.com>
2025-02-22 10:39:39 -0300

    Menu and Start

--- Program.cs
+++ Program.cs
@@ -13,9 +13,34 @@ namespace Stopwath
             Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
             var data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
-            Console.WriteLine(type);
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+            
+            if (type == 'm')
+                multiplier = 60;
+            
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+            
+        }
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+            Console.Clear();
+            Start(time);
         }
         static void Start(int time)
         {
@@ -30,6 +55,8 @@ namespace Stopwath
             }
             Console.Clear();
             Console.WriteLine("Stopwath finalizado");
+            Thread.Sleep(2500);
+            Menu();
         }
     }   
 }
